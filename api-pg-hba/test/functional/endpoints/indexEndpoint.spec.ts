cacab6d08178b132e635bf0cd908f6e825fecc43
Marcelo Hexsel <marcelo.hexsel@hotmail.com>
2020-07-26 15:39:52 -0300

    Index controller functional test

---
+++ api-pg-hba/test/functional/endpoints/indexEndpoint.spec.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import expressApp from "../../../src/application/ExpressApp";
+
+const SEMVER_STRICT_MATCH = /^((([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$/gm
+
+describe('Endpoint: /', () => {
+
+    it('should return application name and version on GET', async () => {
+        let indexResponse = await request(expressApp)
+            .get("/")
+            .expect(200)
+
+        expect(indexResponse.text.split(":")[0].trim()).toMatch(/api-pg-hba/)
+        expect(indexResponse.text.split(":")[1].trim()).toMatch(SEMVER_STRICT_MATCH)
+    });
+
+});
